fdf3857e7f26dc04a8a3d215cd9801dc3e4665b6
user name <user email>
2023-09-08 18:47:45 +0530

    first commit

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Col, Divider, Row } from "antd";
+import TypeCard from "../component/TypeCard";
+import "../CSS/Home.css";
+const Home: React.FC = () => (
+  <div className="home">
+    <div className="container-type" style={{ height: "80vh" }}>
+      <center>
+        <Divider orientation="center" />
+        <Row style={{ height: "32vh", width: "32vw", marginBottom: "4vh" }}>
+          <Col flex={1}>
+            <TypeCard
+              type="Scheme"
+              description="Some information about scheme"
+            />
+          </Col>
+          <Col flex={1}>
+            <TypeCard
+              type="Scholarship"
+              description="Some information about scholarship"
+            />
+          </Col>
+        </Row>
+
+        <Row style={{ height: "32vh", width: "32vw" }}>
+          <Col flex={1}>
+            <TypeCard
+              type="Compensation"
+              description="Some information about Compensation"
+            />
+          </Col>
+          <Col flex={1}>
+            <TypeCard
+              type="Resources"
+              description="Some information about resources"
+            />
+          </Col>
+        </Row>
+      </center>
+    </div>
+  </div>
+);
+
+export default Home;
