9a0aa4c9b01125f0dc1d07396cf80908a15aa3e4
user name <user email>
2023-09-10 18:58:01 +0530

    CheckResponsive-1

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Col, Divider, Row } from "antd";
 import TypeCard from "../component/TypeCard";
 import "../CSS/Home.css";
+import { useMediaQuery } from "react-responsive";
+const isDesktopOrLaptop = useMediaQuery({
+  query: "(min-width: 1224px)",
+});
+const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
 
 const Home: React.FC = () => (
   <div className="App">
@@ -9,44 +16,50 @@ const Home: React.FC = () => (
     <div className="home">
       <div className="container-type" style={{ height: "80vh" }}>
         <center>
-          <Divider orientation="center" />
-          <Row style={{ height: "32vh", width: "32vw", marginBottom: "4vh" }}>
-            <Col flex={1}>
-              <TypeCard
-                type="Scheme"
-                description="Some information about scheme"
-                id="1"
-                url="https://img.freepik.com/premium-vector/books-money-loans-scholarships_603843-825.jpg"
-              />
-            </Col>
-            <Col flex={1}>
-              <TypeCard
-                type="Scholarship"
-                description="Some information about scholarship"
-                id="2"
-                url="/scholarship.jpg"
-              />
-            </Col>
-          </Row>
+          {isDesktopOrLaptop && (
+            <div>
+              <Divider orientation="center" />
+              <Row
+                style={{ height: "32vh", width: "32vw", marginBottom: "4vh" }}
+              >
+                <Col flex={1}>
+                  <TypeCard
+                    type="Scheme"
+                    description="Some information about scheme"
+                    id="1"
+                    url="https://img.freepik.com/premium-vector/books-money-loans-scholarships_603843-825.jpg"
+                  />
+                </Col>
+                <Col flex={1}>
+                  <TypeCard
+                    type="Scholarship"
+                    description="Some information about scholarship"
+                    id="2"
+                    url="/scholarship.jpg"
+                  />
+                </Col>
+              </Row>
 
-          <Row style={{ height: "32vh", width: "32vw" }}>
-            <Col flex={1}>
-              <TypeCard
-                type="Compensation"
-                description="Some information about Compensation"
-                id="3"
-                url="/compensation.jpg"
-              />
-            </Col>
-            <Col flex={1}>
-              <TypeCard
-                type="Resources"
-                description="Some information about resources"
-                id="0"
-                url="/books.jpg"
-              />
-            </Col>
-          </Row>
+              <Row style={{ height: "32vh", width: "32vw" }}>
+                <Col flex={1}>
+                  <TypeCard
+                    type="Compensation"
+                    description="Some information about Compensation"
+                    id="3"
+                    url="/compensation.jpg"
+                  />
+                </Col>
+                <Col flex={1}>
+                  <TypeCard
+                    type="Resources"
+                    description="Some information about resources"
+                    id="0"
+                    url="/books.jpg"
+                  />
+                </Col>
+              </Row>
+            </div>
+          )}
         </center>
       </div>
     </div>
