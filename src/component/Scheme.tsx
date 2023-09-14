639026c981fe6ec247e5fd0ac35ef1d69560f75e
Gurkaran Singh <gurkaran@grevity.in>
2023-09-14 15:59:51 +0530

    fixed few things and changed few things in schemes card

--- src/component/Scheme.tsx
+++ src/component/Scheme.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Col, Collapse, Divider, Row, Typography } from "antd";
 import schemeData from "../data/data.json";
-import parse from "html-react-parser";
 import Collapsible from "./Collapsible";
+import { HomeOutlined, InfoCircleOutlined, InfoOutlined, ManOutlined, WomanOutlined } from "@ant-design/icons";
 interface SchemeProps {
   id: string[];
   type: number;
@@ -26,7 +26,6 @@ const Scheme: React.FC<SchemeProps> = (props): JSX.Element => {
         }
       }
     }
-    console.log(data);
     return data;
   };
 
@@ -34,42 +33,84 @@ const Scheme: React.FC<SchemeProps> = (props): JSX.Element => {
   return interimDataArray.length === 0 ? (
     <h1>No scheme Found</h1>
   ) : (
-    <div style={{ width: "84vw" }}>
-      {interimDataArray.map((interimdata) => (
-        <Collapsible title={JSON.parse(interimdata)["name"]}>
-          <Card
-            key={JSON.parse(interimdata)["id"]}
-            title={<h3>{JSON.parse(interimdata)["name"]}</h3>}
-            bordered={true}
-            style={{
-              width: "42vw",
-              textAlign: "start",
-              backgroundColor: "#fff0ff",
-              margin: "2vh",
-            }}
-          >
-            <h3>Award/Entitlement:</h3>
-            <p>{parse(JSON.parse(interimdata)["award_entitlement"])}</p>
-            <h3>Gender:</h3>
-            <p>{JSON.parse(interimdata)["gender"]}</p>
-            <h3>Max family income:</h3>
-            <p>{JSON.parse(interimdata)["max_income"]}</p>
-            <h3>Residence:</h3>
-            <p>{JSON.parse(interimdata)["residence"]}</p>
-            <h3>Disability:</h3>
-            <p>{JSON.parse(interimdata)["disability"]}</p>
-            <h3>Eligibility:</h3>
-            <p>{parse(JSON.parse(interimdata)["eligibility"])}</p>
-            <h3>Document required:</h3>
-            <p>{parse(JSON.parse(interimdata)["document_required"])}</p>
-            <h3>Application link:</h3>
-            <p>{parse(JSON.parse(interimdata)["application_link"])}</p>
-            <h3>Grievance redressal:</h3>
-            <p>{parse(JSON.parse(interimdata)["grievance_redressal"])}</p>
-          </Card>
-        </Collapsible>
-      ))}
+    <div style={{ width: "64vw" }}>
+      <Collapse
+        items={interimDataArray.map((interimdata, index) => {
+          return {
+            id: (index + 1).toString(),
+            label: JSON.parse(interimdata)["name"],
+            children: <SchemeCard schemeData={interimdata} />,
+          }
+        })}
+      />
     </div>
   );
 };
 export default Scheme;
+
+
+
+const { Title, Text } = Typography;
+const SchemeCard = ({ schemeData }: any) => {
+  
+  const {
+    name,
+    award_entitlement,
+    gender,
+    max_income,
+    residence,
+    disability,
+    eligibility,
+    document_required,
+    application_link,
+    grievance_redressal,
+  } = JSON.parse(schemeData);
+
+  return (
+    <Card
+      key={name}
+      title={<Title style={{ marginTop: 24 }} level={2}>{name}</Title>}
+      bordered
+      style={{
+        textAlign: 'start',
+        backgroundColor: '#f1faff',
+        margin: '2vh',
+      }}
+    >
+      <Title level={4}><InfoCircleOutlined /> Award/Entitlement:</Title>
+      <div dangerouslySetInnerHTML={{ __html: award_entitlement }}></div>
+
+      <Row style={{ marginTop: 12, marginBottom: 12 }} justify={"space-between"}>
+        <Col>
+          <Title level={4}>Gender:</Title>
+          <Text>{gender == "Female" ? <WomanOutlined /> : <ManOutlined />} {gender}</Text>
+        </Col>
+        <Col>
+          <Title level={4}>Max Family Income:</Title>
+          <Text>Rs.{max_income}</Text>
+        </Col>
+        <Col>
+          <Title level={4}>Disability:</Title>
+          <Text>{disability}</Text>
+        </Col>
+      </Row>
+      <Title level={4}><HomeOutlined /> Residence:</Title>
+      <Text>{residence}</Text>
+
+      <Divider />
+
+
+      <Title level={4}>Eligibility:</Title>
+      <div dangerouslySetInnerHTML={{ __html: eligibility }}></div>
+
+      <Title level={4}>Document Required:</Title>
+      <div dangerouslySetInnerHTML={{ __html: document_required }}></div>
+
+      <Title level={4}>Application Link:</Title>
+      <div dangerouslySetInnerHTML={{ __html: application_link }}></div>
+
+      <Title level={4}>Grievance Redressal:</Title>
+      <div dangerouslySetInnerHTML={{ __html: grievance_redressal }}></div>
+    </Card>
+  );
+};
