685ad8c7bf1d9e959e8175f7b4af00a4d7dbb4ef
user name <user email>
2023-11-07 10:06:22 +0530

    Footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,30 +2,37 @@ import { Col, Row } from "antd";
 
 export default function Footer() {
   return (
-    <Row
-      style={{
-        height: "10vh",
-        width: "100vw",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#03051E",
-        color: "white",
-      }}
-    >
-      <Col span={4} style={{ fontSize: "24px" }}>
-        <a
-          href="https://www.facebook.com/DCPCR/"
-          style={{ textDecoration: "none" }}
-        >
-          Facebook
-        </a>
-      </Col>
-      <Col span={4} style={{ fontSize: "24px" }}>
-        <a href="https://www.instagram.com/dcpcr_delhi/">Instagram</a>
-      </Col>
-      <Col span={4} style={{ fontSize: "24px" }}>
-        <a href="https://twitter.com/DCPCR">Twitter</a>
-      </Col>
-    </Row>
+    <div style={{ marginTop: "32px" }}>
+      <Row
+        style={{
+          height: "10vh",
+          width: "100vw",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#03051E",
+          color: "white",
+        }}
+      >
+        <Col span={4} style={{ fontSize: "24px" }}>
+          <a
+            href="https://www.facebook.com/DCPCR/"
+            style={{ textDecoration: "none" }}
+          >
+            Facebook
+          </a>
+        </Col>
+        <Col span={4} style={{ fontSize: "24px" }}>
+          <a href="https://www.instagram.com/dcpcr_delhi/">Instagram</a>
+        </Col>
+        <Col span={4} style={{ fontSize: "24px" }}>
+          <a href="https://twitter.com/DCPCR">Twitter</a>
+        </Col>
+      </Row>
+      <p style={{ color: "white" }}>
+        Delhi commision for protection of child rights
+        <br />
+        &copy; Produced by Britis Asian Trust;
+      </p>
+    </div>
   );
 }
