c45969e02e03cc4cf7c52fffd3123e0a0b681be7
user name <user email>
2023-11-17 12:09:33 +0530

    Navbar adjust

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -55,123 +55,156 @@ export default function NavBar() {
     //     Contact
     //   </NavLink>
     // </nav>
-    <nav className="navbar">
-      <Modal
-        title="Your Feedback"
-        open={isModalOpen}
-        okText={"Submit Feedback"}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Row style={{ marginBottom: 14 }} gutter={[16, 16]}>
-          <Col span={12}>
-            <Input
-              onChange={(e) => setUser(e.target.value)}
-              placeholder="Enter your name..."
-            />
-          </Col>
-          <Col span={12}>
-            <Input
-              onChange={(e) => setPhone(e.target.value)}
-              placeholder="Enter your phone number..."
-            />
-          </Col>
-        </Row>
-        <Input.TextArea
-          onChange={(e) => setFeedback(e.target.value)}
-          placeholder="Enter your feedback here..."
-        />
-      </Modal>
-      <div className="navbar-container container">
-        <input type="checkbox" name="" id="" />
-        <div className="hamburger-lines">
-          <span className="line line1"></span>
-          <span className="line line2"></span>
-          <span className="line line3"></span>
-        </div>
-        <ul className="menu-items" style={{ position: "absolute", right: 32 }}>
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/"
-            >
-              {/* <HomeOutlined />  */}
-              <p className="navbarItem">Home</p>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/about"
-            >
-              {/* <InfoCircleOutlined />  */}
-              <p className="navbarItem">About</p>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/imp-links"
-            >
-              {/* <FormOutlined /> */}
-              <p className="navbarItem">Resources</p>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/compensation"
-            >
-              {/* <PhoneOutlined />  */}
-              <p className="navbarItem">Compensation</p>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/essentialDocs"
-            >
-              {/* <PhoneOutlined />  */}
-              <p className="navbarItem">Essential Docs</p>
-            </NavLink>
-          </li>
-          <li>
-            <div
-              style={{ cursor: "pointer" }}
-              onClick={() => setIsModalOpen(true)}
-            >
-              {/* <MessageOutlined />  */}
-              <p className="navbarItem" style={{ fontWeight: 500 }}>
-                Feedback
-              </p>
-            </div>
-          </li>
-          <li>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "#1890ff" : "inherit",
-              })}
-              to="/contact"
-            >
-              {/* <PhoneOutlined />  */}
-              <p className="navbarItem">Contact Us</p>
-            </NavLink>
-          </li>
-        </ul>
+    <div>
+      <div style={{ height: "120px" }}>
         <h1 className="logo">
+          {/* <NavLink to="/"> */}
+          <img
+            className="logoIcon"
+            src="../logo.jpeg"
+            alt="LOGO"
+            style={{ zIndex: 101 }}
+          />
+          <p
+            className="logoOfWebsite"
+            style={{
+              // fontSize: "1.5rem",
+              display: "inline",
+              alignItems: "start",
+              position: "absolute",
+              top: "20px",
+              marginTop: "20px",
+              color: "#731816",
+              zIndex: 101,
+            }}
+          >
+            Delhi Commission for Protection of Child Rights
+            <br />
+            (DCPCR) Government of NCT of Delhi
+          </p>
+          {/* </NavLink> */}
+        </h1>
+      </div>
+      <nav className="navbar">
+        <Modal
+          title="Your Feedback"
+          open={isModalOpen}
+          okText={"Submit Feedback"}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <Row style={{ marginBottom: 14 }} gutter={[16, 16]}>
+            <Col span={12}>
+              <Input
+                onChange={(e) => setUser(e.target.value)}
+                placeholder="Enter your name..."
+              />
+            </Col>
+            <Col span={12}>
+              <Input
+                onChange={(e) => setPhone(e.target.value)}
+                placeholder="Enter your phone number..."
+              />
+            </Col>
+          </Row>
+          <Input.TextArea
+            onChange={(e) => setFeedback(e.target.value)}
+            placeholder="Enter your feedback here..."
+          />
+        </Modal>
+        <div className="navbar-container container">
+          <input type="checkbox" name="" id="" />
+          <div className="hamburger-lines">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>
+          <ul
+            className="menu-items"
+            style={{ position: "absolute", right: 32 }}
+          >
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/"
+              >
+                {/* <HomeOutlined />  */}
+                <p className="navbarItem">Home</p>
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/about"
+              >
+                {/* <InfoCircleOutlined />  */}
+                <p className="navbarItem">About</p>
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/imp-links"
+              >
+                {/* <FormOutlined /> */}
+                <p className="navbarItem">Resources</p>
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/compensation"
+              >
+                {/* <PhoneOutlined />  */}
+                <p className="navbarItem">Compensation</p>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/essentialDocs"
+              >
+                {/* <PhoneOutlined />  */}
+                <p className="navbarItem">Essential Docs</p>
+              </NavLink>
+            </li>
+            <li>
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => setIsModalOpen(true)}
+              >
+                {/* <MessageOutlined />  */}
+                <p className="navbarItem" style={{ fontWeight: 500 }}>
+                  Feedback
+                </p>
+              </div>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "#1890ff" : "inherit",
+                })}
+                to="/contact"
+              >
+                {/* <PhoneOutlined />  */}
+                <p className="navbarItem">Contact Us</p>
+              </NavLink>
+            </li>
+          </ul>
+          {/* <h1 className="logo">
           <NavLink to="/">
             <img
               className="logoIcon"
@@ -195,8 +228,9 @@ export default function NavBar() {
               (DCPCR) Government of NCT of Delhi
             </p>
           </NavLink>
-        </h1>
-      </div>
-    </nav>
+        </h1> */}
+        </div>
+      </nav>
+    </div>
   );
 }
