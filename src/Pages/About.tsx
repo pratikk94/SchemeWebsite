78f11e98798b9fab018bea12d2da1dae2adb1976
user name <user email>
2023-11-21 17:07:34 +0530

    Mobile firendly page-3.2_BG

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -18,7 +18,7 @@ const AboutUs: React.FC = () => {
                 margin: "2vw",
                 fontSize: "48px",
                 fontWeight: 900,
-                marginTop: "60vh",
+                marginTop: "30vh",
                 color: "#1A196F",
               }}
             >
--- src/Pages/ImpLinks.tsx
+++ src/Pages/ImpLinks.tsx
@@ -313,7 +313,7 @@ const ImpLinks: React.FC = () => (
         <center>
           <h1
             style={{
-              marginTop: "10vh",
+              marginTop: "-10vh",
               marginBottom: "32px",
               fontSize: "48px",
               color: "#1A196F",
